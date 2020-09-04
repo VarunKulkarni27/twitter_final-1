f267a6ac446e7d35ba9adc5ed608d292ab0d1172
VarunKulkarni27 <70739078+VarunKulkarni27@users.noreply.github.com>
2020-09-04 18:50:22 +0530

    Update mainbot.py

--- mainbot.py
+++ mainbot.py
@@ -30,9 +30,9 @@ def follower_count(user):
 api = create_api()
 
 while True:
-    user = api.get_user('the_ameen_manna')
-    api.update_profile(name=f'AMEEN|{follower_count(user)} Followers')
-    print(f'Updating Twitter Name : AMEEN|{follower_count(user)} Followers')
+    user = api.get_user('VarunKu69696015')
+    api.update_profile(name=f'Varun Kulkarni|{follower_count(user)} Followers')
+    print(f'Updating Twitter Name : Varun Kulkarni|{follower_count(user)} Followers')
     print('Waiting to refresh')
     time.sleep(60)
   
